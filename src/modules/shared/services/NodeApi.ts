323244184f82fba8297895f7ad0c05ea8584c238
Sandro <sjr1999@protonmail.com>
2023-12-14 14:31:17 -0300

    Change API URLs to API Gateway URLs

--- src/modules/shared/services/NodeApi.ts
+++ src/modules/shared/services/NodeApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const SpringApi = axios.create({
-    baseURL: 'http://172.20.222.48:3000',
+    baseURL: 'http://172.20.222.48:80/api/iot',
 }) ;
 
 export default SpringApi;
--- src/modules/shared/services/SpringApi.ts
+++ src/modules/shared/services/SpringApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const SpringApi = axios.create({
-    baseURL: 'http://172.20.222.48:8080',
+    baseURL: 'http://172.20.222.48:80/api/core',
 }) ;
 
 export default SpringApi;
